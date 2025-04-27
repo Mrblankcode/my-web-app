a438340dc59aff88b9bd54d3dd282b5ee3694fe2
Mrblankcode <bruvmeguled@gmail.com>
2025-04-26 21:27:04 -0400

    Update website.py with new content

---
+++ website.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+
+# --------HEADER SECTION--------
+st.subheader("Hi, I'm Guled a rising theoretical physicist :wave:")
+st.title("My Webpage")
+st.write("I am passionate about physics and mathematics, and I am currently pursuing a degree in theoretical physcis.")
+st.write("[Credit to My Dad >](http://www.pioneerscitech.com/upload/2018/07/alixxx_200_200.png)")
