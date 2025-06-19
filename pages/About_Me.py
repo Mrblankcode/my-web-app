d0357d780295799b4b95f7ae21065f16bb00ce79
Mrblankcode <bruvmeguled@gmail.com>
2025-06-19 11:42:53 -0400

    Update website

--- pages/About_Me.py
+++ pages/About_Me.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+st.set_page_config(page_title="AboutMe", page_icon=":tada:", layout="wide")
 
 
 # --------HEADER SECTION--------
@@ -9,8 +9,10 @@ st.title("My Webpage")
 st.write("I am passionate about physics and mathematics, and I am currently pursuing a degree in theoretical physics.")
 st.write("I am also interested in programming and data science, and I am always looking for new challenges to tackle.")
 
-# Comment out the image line for testing
-st.write("[Credit to my dad>](http://www.pioneerscitech.com/upload/2018/07/alixxx_200_200.png)")
+guled_image = "WIN_20250619_09_12_35_Pro.jpg"
+st.write("This is a picture of me:")
+st.image("WIN_20250619_09_12_35_Pro.jpg", caption="Guled's picture", use_column_width=True)
+
 url = "http://www.pioneerscitech.com/u/nassiba"
 st.markdown(
     f"Check out this [link]({url}) to my father's science and technology website!")
