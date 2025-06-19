cb3ba09e5950f40f0fdf91dcae00c3f62bbbe4bc
Mrblankcode <bruvmeguled@gmail.com>
2025-06-19 11:45:30 -0400

    Update website

--- pages/About_Me.py
+++ pages/About_Me.py
@@ -9,9 +9,10 @@ st.title("My Webpage")
 st.write("I am passionate about physics and mathematics, and I am currently pursuing a degree in theoretical physics.")
 st.write("I am also interested in programming and data science, and I am always looking for new challenges to tackle.")
 
-guled_image = "WIN_20250619_09_12_35_Pro.jpg"
+
 st.write("This is a picture of me:")
-st.image("WIN_20250619_09_12_35_Pro.jpg", caption="Guled's picture", use_column_width=True)
+st.image("WIN_20250619_09_12_35_Pro.jpg",
+         caption="Guled's picture", use_container_width=True)
 
 url = "http://www.pioneerscitech.com/u/nassiba"
 st.markdown(
