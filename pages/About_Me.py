c3b5d26457c4b1acc95defec8b0975edcaa4f5fd
Mrblankcode <bruvmeguled@gmail.com>
2025-06-26 09:02:45 -0400

    Update About Me and Projects pages

--- pages/About_Me.py
+++ pages/About_Me.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 st.set_page_config(page_title="AboutMe", page_icon=":tada:", layout="wide")
 
@@ -11,9 +12,44 @@ st.write("I am also interested in programming and data science, and I am always 
 
 
 st.write("This is a picture of me:")
-st.image("WIN_20250619_09_12_35_Pro.jpg",
-         caption="Guled's picture", use_container_width=True)
+image_path = "pages/guled.jpg"
+if os.path.exists(image_path):
+    st.image(image_path, caption="Guled's picture", use_container_width=True)
+else:
+    st.warning(f"Image file '{image_path}' not found.")
 
 url = "http://www.pioneerscitech.com/u/nassiba"
 st.markdown(
     f"Check out this [link]({url}) to my father's science and technology website!")
+url2 = "https://www.youtube.com/@gulednassib7360"
+st.markdown(
+    f"Check out this [link]({url2}) to my YouTube channel!")
+
+
+# --------CREATOR SECTION--------
+st.title("Creators")
+# List of creators
+creators = [
+    {
+        "name": "Guled Nassib",
+        "role": "Theoretical Physicist",
+        "image": "pages/guled.jpg",
+        "desc": "Guled is passionate about physics and mathematics."
+    },
+    {
+        "name": "Ali Nassib",
+        "role": "computer scientist",
+        "image": "pages/ali.png",
+        "desc": "Ali specializes in radars and works for the FCC."
+    },
+    # Add more creators as needed
+]
+
+# Slider to select creator
+index = st.slider("Slide to see the creators", 0, len(creators)-1, 0)
+
+creator = creators[index]
+st.image(creator["image"], caption=creator["name"], use_container_width=True)
+st.subheader(creator["name"])
+st.write(f"**Role:** {creator['role']}")
+st.write(creator["desc"])
